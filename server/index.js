1d720ad8af07c1199fc2a5718c8847a9b7daa42d
Abde <azzaoui03dev@gmail.com>
2025-05-08 14:04:05 +0100

    fix: with-credentials

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
+  withCredentials: true,
 }));
 app.use(cookieParser() );
 app.use(express.urlencoded({extended: true}));
